622461cfe7083f4887aac7adc7fbfb46a682ecef
Alex Zhidkov <sashazudkov@gmail.com>
2022-10-04 23:08:11 +0300

    upd

--- main.rb
+++ main.rb
@@ -8,12 +8,12 @@ def alphabet_position(string)
     end
   }
 
-  puts result
+  result
 end
 
 # example from the Discord
 
-alphabet_position("The sunset sets at twelve o' clock.")
+puts "Result: " + alphabet_position("The sunset sets at twelve o' clock.")
 
 # should be equal
-puts '20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11'
+puts 'Correct answer: 20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11'
